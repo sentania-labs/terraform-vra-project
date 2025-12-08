28ae1b4b83c02e0e6a24d68be3820f80a3d240ae
Scott R Bowe <scottb@sentania.net>
2025-12-08 14:50:53 -0600

    add an example

---
+++ examples/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    vra = {
+      source  = "vmware/vra"
+      version = "~> 0.11"
+    }
+  }
+}
+
+module "project" {
+  source = "../../"
+  name        = var.project_name
+  description = var.description
+
+  administrators = var.administrators
+  members        = var.members
+  basename       = var.basename
+  cloud_zone_ids = var.cloud_zone_ids
+}
