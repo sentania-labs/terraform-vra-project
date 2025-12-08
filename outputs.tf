06d9f5ec370accc7b4de39b46904b1c778051057
Scott R Bowe <scottb@sentania.net>
2025-12-08 15:09:17 -0600

    documentation updates

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,5 @@
 output "project" {
+  description = "An object that allows referencing to the Automation Project created by name or id"
   value = {
     "name" = vra_project.this.name,
     "id"   = vra_project.this.id
